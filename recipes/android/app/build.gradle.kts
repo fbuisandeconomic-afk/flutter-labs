60f63132117f0542d3a1b471c5658b35c440fc44
asror <fbuisandeconomic@gmail.com>
2026-02-19 16:32:57 +0300

    Приложение собрано

--- recipes/android/app/build.gradle.kts
+++ recipes/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.recipes"
+        applicationId = "com.bulochka.mir_receptov"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
